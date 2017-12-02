79a9ae45e5054dd64b0886cdb0100e2d92096d7d
m-hoshiba <31546105+m-hoshiba@users.noreply.github.com>
2017-12-02 15:53:30 +0900

    a

---
+++ New Unity Project/Assets/PosView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PosView : MonoBehaviour {
+    public Text position;
+    private float x = 0;
+    private float y = 0;
+    private float z = 0;
+    [SerializeField] private Transform target;
+
+
+    // Use this for initialization
+    void Start () {
+        RefreshPosition();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        x = target.position.x;
+        y = target.position.y;
+        z = target.position.z;
+        RefreshPosition();
+    }
+
+    void RefreshPosition()
+    {
+        position.text = "POSITION X : " + x + " Y : " + y + " Z : " + z;
+
+    }
+
+}
---
+++ New Unity Project/Assets/testopencv.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using OpenCvSharp;
+
+
+public class testopencv : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Mat src = new Mat("C:/Users/moto/Documents/New Unity Project/Assets/su-152_1920x1080_noc_ru.jpg", ImreadModes.GrayScale);
+        // Mat src = Cv2.ImRead("lenna.png", ImreadModes.GrayScale);
+        Mat dst = new Mat();
+
+        Cv2.Canny(src, dst, 50, 200);
+        using (new Window("src image", src))
+        using (new Window("dst image", dst))
+        {
+            Cv2.WaitKey();
+
+        }
+
+        // Update is called once per frame
+		
+	}
+}
