dc5cc09f0b0b3c0cc05046e03de46176ae082d44
m-hoshiba <31546105+m-hoshiba@users.noreply.github.com>
2017-12-02 16:40:19 +0900

    screenshot

---
+++ New Unity Project/Assets/Screenshot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour {
+
+    private string directryPath = "Assets/Screenshot/";
+    [SerializeField] private string FileName = "screenshot.png";
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ScreenCapture.CaptureScreenshot(directryPath + FileName);
+        }
+
+    }
+}
